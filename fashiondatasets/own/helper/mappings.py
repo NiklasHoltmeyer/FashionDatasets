0855d8bee94a823fddd1ad4f2f8382d218c1a755
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-11-12 10:09:43 +0100

    preprocess

--- fashiondatasets/own/helper/mappings.py
+++ fashiondatasets/own/helper/mappings.py
@@ -1,15 +1,20 @@
 import tensorflow as tf
 
 
-def preprocess_image(img_shape, preprocess_img=None, channels=3, dtype=tf.float32):
+def preprocess_image(img_shape, preprocess_img=None, augmentation=None, channels=3, dtype=tf.float32):
     @tf.function
     def __call__(filename):
         image_string = tf.io.read_file(filename)
         image = tf.image.decode_jpeg(image_string, channels=channels)
         image = tf.image.convert_image_dtype(image, dtype)
         image = tf.image.resize(image, img_shape)
+
         if preprocess_img:
             image = preprocess_img(image)
+
+        if augmentation:
+            image = augmentation(image)
+
         return image
 
     return __call__
