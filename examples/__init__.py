ea9dbb7c2d6b6c4c949a8f9b8ea29734d9bec832
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-11-30 11:56:13 +0100

    dl

---
+++ examples/__init__.py
@@ -0,0 +1,11 @@
+from fashiondatasets.utils.logger.defaultLogger import defaultLogger
+
+l = defaultLogger()
+l.error("test")
+l.error("test")
+l.error("test")
+
+l = defaultLogger()
+l.error("test")
+l.error("test")
+l.error("test")
