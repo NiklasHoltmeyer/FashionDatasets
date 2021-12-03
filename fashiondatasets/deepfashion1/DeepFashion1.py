63644c07a4ca41b6bccf46aeb165cf0b9b0cf392
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-12-03 17:06:07 +0100

    chunk size

--- fashiondatasets/deepfashion1/DeepFashion1.py
+++ fashiondatasets/deepfashion1/DeepFashion1.py
@@ -163,13 +163,12 @@ class DeepFashion1Dataset:
         if len(missing_embeddings) < 1:
             return
 
-        c_size = 18_000
-
-        if len(missing_embeddings) > c_size:
-            missing_chunked = np.array_split(missing_embeddings, len(missing_embeddings) // c_size)
+        if len(missing_embeddings) > 8:
+            missing_chunked = np.array_split(missing_embeddings, 8)
         else:
             missing_chunked = [missing_embeddings]
 
+        logger.info("_build_missing_embeddings::encode_paths")
         for chunk_missing in missing_chunked:
             self.pair_gen.pair_gen.encode_paths([chunk_missing], retrieve_paths_fn=lambda d: d,
                                                 assert_saving=True, skip_filter=True)
