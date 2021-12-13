c37e004ae6f09c367cecfe54638c279371a31dd8
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-12-13 11:53:18 +0100

    df

--- fashiondatasets/deepfashion1/helper/deep_fashion_1_pairs_generator.py
+++ fashiondatasets/deepfashion1/helper/deep_fashion_1_pairs_generator.py
@@ -238,6 +238,7 @@ class DeepFashion1PairsGenerator:
     def build_jpg_path(self, npy_full_path):
         print("0")
         print(self.image_base_path)
+        print(npy_full_path)
         print(self.image_base_path / npy_full_path.split(os.path.sep)[-1].replace(".npy", ".jpg").replace("(-)", "/"))
         raise Exception("bla")
         return self.image_base_path / npy_full_path.split(os.path.sep)[-1].replace(".npy", ".jpg").replace("(-)", "/")
