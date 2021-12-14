ce6bee044af01b5e802b1c66c2900813ebf20dc7
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2021-12-14 15:27:49 +0100

    chunk size

---
+++ fashiondatasets/deepfashion1/helper/notebook.py
@@ -0,0 +1,105 @@
+import os
+import pickle
+from pathlib import Path
+
+from fashiondatasets.deepfashion1.DeepFashion1CBIR import DeepFashion1CBIR
+
+def map_top_k(result, k=20):
+    matches = result["distances"]["matches"]
+    return min([x[-1] for x in matches]) < k
+
+
+def calc_top_k_from_distances(results):
+    _result = results["result"]
+    top_20 = map_top_k
+
+    top_100 = lambda r: map_top_k(r, k=100)
+
+    top_20_value = 100 * sum(map(top_20, _result)) / len(_result)
+    top_100_value = 100 * sum(map(top_100, _result)) / len(_result)
+
+    top_values = {
+        "embedding_path": results["embedding_path"],
+        "top_20": top_20_value,
+        "top_100": top_100_value
+    }
+
+    return top_values
+
+def distances_existence_filtered(path, exist=False):
+    x = list(map(lambda p: os.path.join(path, p), os.listdir(path)))
+    x = list(filter(lambda p: os.path.isdir(p), x))
+    x = list(map(lambda p: (p, os.path.join(p, "distances.pkl")), x))
+
+    if exist:
+        existence_filter = lambda p: Path(p[1]).exists()
+    else:
+        existence_filter = lambda p: not Path(p[1]).exists()
+
+    x = filter(existence_filter, x)
+    x = list(x)
+
+    return x
+
+def calc_top_k_from_embeddings(base_path, embedding_path, idx=None, pickle_result=True, raise_exception=True):
+    idx = idx or 0
+
+    print(embedding_path)
+    distances_path = os.path.join(embedding_path, "distances.pkl")
+
+    cbir = DeepFashion1CBIR(base_path,
+                            model=None,
+                            embedding_path=embedding_path,
+                            disable_output=idx > 0)
+    result = {
+        "embedding_path": embedding_path
+    }
+
+    try:
+        distance_walker = cbir.walk_distances()
+        result["result"] = list(distance_walker)
+    except Exception as e:
+        if raise_exception:
+            raise e
+        result["exception"] = e
+
+    if not pickle_result:
+        return result
+
+    try:
+        with open(distances_path, "wb") as f:
+            pickle.dump(result, f)
+    except:
+        print("Pickle, failed")
+
+    return result
+
+def load_distances(embedding_path):
+    distance_paths = [x[1] for x in distances_existence_filtered(embedding_path, exist=True)]
+    d = []
+    for d_p in distance_paths:
+        with open(d_p, "rb") as f:
+            data = pickle.load(f)
+            d.append(data)
+    return d
+
+def load_results(embedding_path, verbose=False):
+    distances = load_distances(embedding_path)
+
+    results = []
+
+    for distance in distances:
+        top_values = calc_top_k_from_distances(distance)
+
+        if verbose:
+            top_values.update(distance)
+
+        results.append(top_values)
+
+    return results
+
+if __name__ == "__main__":
+    emb_base = r"C:\workspace"
+    print(load_results(emb_base))
+
+
